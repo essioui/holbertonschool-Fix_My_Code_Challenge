8b5ea1880ac65d54b10436c1c2f59b7e5c2402d2
essioui <salahessioui@gmail.com>
2024-09-11 14:04:16 +0100

    delete_dnodeint

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,10 +1,11 @@
-#include "lists.h"
+#include <string.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
- * @head: A pointer to the first element of the list
+ * @head: A pointer to the pointer to the first element of a list
  * @index: The index of the node to delete
  *
  * Return: 1 on success, -1 on failure
@@ -12,46 +13,35 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *current;
+    dlistint_t *tmp;
     unsigned int i;
 
     if (*head == NULL)
-    {
         return (-1);
-    }
 
     current = *head;
 
     /* Traverse to the node to delete */
-    for (i = 0; current != NULL && i < index; i++)
+    for (i = 0; i < index && current != NULL; i++)
     {
         current = current->next;
     }
 
-    /* If the node was not found */
+    /* If index is out of range */
     if (current == NULL)
-    {
         return (-1);
-    }
 
-    /* Update the previous node's next if it's not the first node */
+    /* If deleting the head node */
     if (current->prev != NULL)
-    {
         current->prev->next = current->next;
-    }
     else
-    {
-        /* Update the head if deleting the first node */
         *head = current->next;
-    }
 
-    /* Update the next node's prev if it's not NULL */
     if (current->next != NULL)
-    {
         current->next->prev = current->prev;
-    }
 
-    /* Free the memory of the node to be deleted */
-    free(current);
+    tmp = current;
+    free(tmp);
 
     return (1);
 }
